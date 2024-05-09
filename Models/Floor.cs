e41607cc9c0476c25d38ff1dacb1fc438f9678ae
Line Andersen <linea.89@outlook.dk>
2024-05-09 03:10:00 +0200

    Floor again

---
+++ Models/Floor.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalProject.Models
+{
+    public class Floor
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int SortOrder { get; set; } = 0;
+
+        public int MoveId { get; set; } // Required foreign key property
+        public Move Move { get; set; } = null!; // Required reference navigation to principal
+    }
+}
